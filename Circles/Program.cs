0fedc5c93142cfc9b58e42c541db2818309eb741
Chnapak <matej.hladecek@gmail.com>
2024-07-12 13:58:30 +0200

    Created a repository

---
+++ Circles/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Podminky
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new PodminkyApp();
+            app.Run();
+        }
+    }
+}
